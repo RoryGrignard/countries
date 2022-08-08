103009763a7496e35ab5667ceffa39d7f52043cc
Rory <rgrignard@gmail.com>
2022-08-08 16:36:24 +0200

    CountryGrid template and styling

--- src/components/CountryGrid.vue
+++ src/components/CountryGrid.vue
@@ -5,18 +5,6 @@
                 <h3>Hello World</h3>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, modi in sapiente dolores aut sed incidunt necessitatibus? Animi recusandae, necessitatibus non qui minima itaque dignissimos voluptatem iure, atque culpa nobis.</p>
             </div>
-            <div class="country-grid__item">
-                <h3>Hello World</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, modi in sapiente dolores aut sed incidunt necessitatibus? Animi recusandae, necessitatibus non qui minima itaque dignissimos voluptatem iure, atque culpa nobis.</p>
-            </div>
-            <div class="country-grid__item">
-                <h3>Hello World</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, modi in sapiente dolores aut sed incidunt necessitatibus? Animi recusandae, necessitatibus non qui minima itaque dignissimos voluptatem iure, atque culpa nobis.</p>
-            </div>
-            <div class="country-grid__item">
-                <h3>Hello World</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, modi in sapiente dolores aut sed incidunt necessitatibus? Animi recusandae, necessitatibus non qui minima itaque dignissimos voluptatem iure, atque culpa nobis.</p>
-            </div>
         </div>
     </main>
 </template>
@@ -37,6 +25,20 @@
     }
     &.dark {
         background-color: $c-cod-grey;
+    }
+    &__container {
+        display: flex;
+        flex-wrap: wrap;
+        gap: $gtr;
+    }
+    &__item {
+        width: calc((100% - 20px) / 2);
+        @media (min-width: 767px) {
+            width: calc((100% - 60px) / 4);
+        }
+        @media (min-width: 992px) {
+            width: calc((100% - 100px) / 6);
+        }
     }
 }
 </style>
